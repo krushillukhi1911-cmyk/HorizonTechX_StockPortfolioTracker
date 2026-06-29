7ddec55ce2daeec62c626f19702b32fd3287238b
krushillukhi1911-cmyk <krushillukhi1911@gmail.com>
2026-06-29 14:03:51 +0530

    Add files via upload

---
+++ stock_prices.py
@@ -0,0 +1,12 @@
+stocks = {
+    "AAPL": 180,
+    "TSLA": 250,
+    "GOOGL": 140,
+    "MSFT": 420,
+    "AMZN": 190,
+    "META": 500,
+    "NFLX": 650,
+    "NVDA": 1200,
+    "AMD": 175,
+    "INTC": 32
+}
